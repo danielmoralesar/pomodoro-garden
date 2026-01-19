f79eabdfe0e38d3ca27ffd6d69e54417ac434a2c
danielmoralesar <danielmoralestrb@gmail.com>
2026-01-19 10:51:59 +0100

    update HarvestPlantDAO.php

--- app/repositories/HarvestPlantDAO.php
+++ app/repositories/HarvestPlantDAO.php
@@ -39,7 +39,7 @@ class HarvestPlantDAO{
         }
     }
 
-    public static function select(string $data, string $type){
+    public static function select(string $data, string $type): ?HarvestPlant{
         if (!checkPlantDataType($type)){
             return null;
         }
@@ -71,15 +71,74 @@ class HarvestPlantDAO{
     }
 
 
-    public static function selectAll(HarvestPlant $harvestPlant){
-
+    public static function selectAll(): ?array{
+        $plants = null;
+        $conn = CoreDB::getConn();
+        $query = "SELECT * FROM plants";
+        $prSt = $conn->prepare($query);
+        $prSt->execute();
+        $results = $prSt->get_result();
+        while (($row = $results->fetch_assoc()) != null){
+            $plants[] = new HarvestPlant(
+                $row['title'],
+                $row['description'],
+                $row['plant_pic'],
+                $row['deadline'],
+                $row['planted_day'],
+                $row['plant_state'],
+                $row['previous_state'],
+                $row['task_completed'],
+                $row['health_points'],
+                $row['id']
+            );
+        }
+        return $plants;
     }
 
-    public static function update(HarvestPlant $harvestPlant){
+    public static function update(HarvestPlant $harvestPlant): ?HarvestPlant{
+        $plantDB = HarvestPlantDAO::select($harvestPlant->getTitle(), "title");
+        if ($harvestPlant !== HarvestPlantDAO::select($harvestPlant->getId(), "id") &&
+        (!$plantDB || $plantDB->getId() == $harvestPlant->getId())){
+            $conn = CoreDB::getConn();
+            $query = "UPDATE plants SET title = ?, description = ?, plant_pic = ?, deadline = ?, planted_day = ?, plant_state = ?, previous_state = ?, task_completed = ?, health_points = ? WHERE id = ?";
+            $prSt = $conn->prepare($query);
 
+            $title = $harvestPlant->getTitle();
+            $description = $harvestPlant->getDescription();
+            $plantPic = $harvestPlant->getPlantPic();
+            $deadLine = $harvestPlant->getDeadLine();
+            $plantedDay = $harvestPlant->getPlantedDay();
+            $plantState = $harvestPlant->getPlantStateAsString();
+            $previousState = $harvestPlant->getPreviousStateAsString();
+            $taskCompleted = $harvestPlant->getTaskCompleted();
+            $healthPoints = $harvestPlant->getHealthPoints();
+            $id = $harvestPlant->getId();
+
+            $prSt->bind_param("sssiisssii",
+                $title, $description, $plantPic, $deadLine, $plantedDay, $plantState, $previousState, $taskCompleted, $healthPoints, $id);
+
+            try {
+                $prSt->execute();
+                $conn->close();
+                return $harvestPlant;
+            } catch (Exception $e) {
+                $conn->close();
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 
     public static function delete(HarvestPlant $harvestPlant){
-
+        $conn = CoreDB::getConn();
+        $query = "DELETE FROM plants WHERE id = ?";
+        $prSt = $conn->prepare($query);
+        $id = $harvestPlant->getId();
+        $prSt->bind_param("i", $id);
+        $prSt->execute();
+        $result = $prSt->affected_rows > 0;
+        $conn->close();
+        return $result;
     }
 }
